d711d7bf0dff62ac5aa29cc1737be2a5a3c6fc11
Carvi3 <andrewcarvajal38@gmail.com>
2024-03-07 11:30:06 -0500

    first commit from cli

---
+++ src/main/java/carv/App.java
@@ -0,0 +1,52 @@
+package carv;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.concurrent.TimeUnit;
+
+
+
+import org.bson.Document;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try{
+            String url = "mongodb://Carvi3:password@127.0.0.1:27017";
+            MongoClient mongoClient = MongoClients.create(MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(url))
+                .applyToSocketSettings(builder -> builder.connectTimeout(5, TimeUnit.SECONDS))
+                .build());
+
+            MongoDatabase admin = mongoClient.getDatabase("admin");
+            
+
+            MongoDatabase database = mongoClient.getDatabase("demo");
+
+    
+            MongoCollection<Document> collection = database.getCollection("associates");
+    
+            Document document = new Document();
+            document.append("firstName", "test");
+            document.append("lastName", "user");
+            collection.insertOne(document);
+
+
+        }catch(Exception e){
+            System.out.println("Something Went wrong");
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Finished Program");
+    }
+}
